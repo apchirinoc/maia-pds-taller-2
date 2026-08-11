d503aedc3a6986b2bbd1b42573972a1d50587991
Alex Chirino <alexchirino3@gmail.com>
2026-08-10 20:46:24 -0500

    agrega version en titulo del dashboard

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = dash.Dash(
     __name__,
     meta_tags=[{"name": "viewport", "content": "width=device-width, initial-scale=1"}],
 )
-app.title = "Dashboard energia"
+app.title = "Dashboard energia v0.1.0"
 
 server = app.server
 app.config.suppress_callback_exceptions = True
